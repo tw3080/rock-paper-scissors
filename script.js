91300157be5cadfe538bcf7db004f2a0fbbdf5df
tw3080 <tw3080@gmail.com>
2023-02-13 14:06:31 -0800

    Create function that plays a single round of Rock, Paper, Scissors

--- script.js
+++ script.js
@@ -1,13 +1,61 @@
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3) + 1;
+    let choice = Math.floor(Math.random() * 3) + 1;
+    console.log('Computer choice: ' + choice);
 
-    if (computerChoice === 1) {
-        return 'Rock';
-    } else if (computerChoice === 2) {
-        return 'Paper';
+    // switch (choice) {
+    //     case 1:
+    //         return 'rock';
+    //         break;
+    //     case 2:
+    //         return 'paper';
+    //         break;
+    //     case 3:
+    //         return 'scissors';
+    //         break;
+    // }
+
+    if (choice === 1) {
+        return 'rock';
+    } else if (choice === 2) {
+        return 'paper';
     } else {
-        return 'Scissors';
+        return 'scissors';
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    let playerChoice = playerSelection.toLowerCase();
+    let computerChoice = computerSelection.toLowerCase();
+    console.log('Player choice: ' + playerChoice);
+
+    if (playerChoice === 'rock' && computerChoice === 'scissors') {
+        return 'You win! Rock beats scissors.';
+    } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+        return 'You win! Paper beats rock.';
+    } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+        return 'You win! Scissors beats paper.';
+    } else if (playerChoice === 'rock' && computerChoice === 'paper') {
+        return 'You lose! Paper beats rock.';
+    } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
+        return 'You lose! Scissors beats paper.';
+    } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
+        return 'You lose! Rock beats scissors.';
+    } else {
+        return 'Tie!';
+    }
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log('Computer choice: ' + computerSelection);
+console.log(playRound(playerSelection, computerSelection));
+
+// player win conditions:
+//     rock beats scissors
+//     paper beats rock
+//     scissors beats paper
+
+// player lose conditions:
+//     rock loses to paper
+//     paper loses to scissors
+//     scissors loses to rock
